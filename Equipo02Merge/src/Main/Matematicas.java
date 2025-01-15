bb84faf3f3856320d5474e89989bffb7fc586482
JoaquinDomingo <dojoaquindo@gmail.com>
2025-01-15 14:35:49 +0100

    V.0.0.1. Creo clase principal

---
+++ Equipo02Merge/src/Main/Matematicas.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Main;
+
+/**
+ * 
+ */
+public class Matematicas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		System.out.println("Proyecto del equipo 2");
+	}
+
+}
