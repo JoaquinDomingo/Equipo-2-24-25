28791914f00e02c37daa3a4dddd786e072d1a9e2
JoaquinDomingo <dojoaquindo@gmail.com>
2025-01-22 14:16:48 +0100

    Version 0.0.0.3 Metodo Principal y EsPar

--- Equipo02Merge/src/Main/Matematicas.java
+++ Equipo02Merge/src/Main/Matematicas.java
@@ -3,6 +3,8 @@
  */
 package Main;
 
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -15,6 +17,34 @@ public class Matematicas {
 		// TODO Auto-generated method stub
 			
 		System.out.println("Proyecto del equipo 2");
+		Scanner scanner = new Scanner(System.in);
+		int opcion;
+		do {
+			System.out.println("Menu de opciones:");
+			System.out.println("1.-Verificar si un numero es par:");
+			System.out.println("2.-Verificar si es divisble entre tres :");
+			System.out.println("3.-Verificar si es divisible entre cinco :");
+			System.out.println("4.- Salir");
+			System.out.println("Seleccione una opción");
+			opcion = scanner.nextInt();
+			
+			if (opcion >= 1 && opcion <= 3) {
+				System.out.print("Ingrese un número:");
+				int numero = scanner.nextInt();
+				switch (opcion) {
+					case 1: System.out.println("¿Es par? " + esPar(numero));
+						break;
+					case 2: System.out.println("Es divisible por tres?" + esDivisiblePorTres(numero));
+						break;
+					case 3: System.out.println("Es divisible por Cinco?" + esDivisiblePorCinco(numeero));
+						break;	
+				}
+			}
+		} while (opcion != 4);
+		
+		public static boolean esPar(int numero) {
+			return numero%2 == 0;
+		}
 	}
 
 }
